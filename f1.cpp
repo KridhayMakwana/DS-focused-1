11befa22d7eb5bb589b5fd690cf284ca8afb43a2
Kridhay Makwana <Kmakwana1833@conestogac.on.ca>
2024-05-17 21:08:19 -0400

    User input loop

--- f1.cpp
+++ f1.cpp
@@ -24,3 +24,28 @@ int main()                       // Main function
 {
 
     printf("Enter flight information:\n");            // Prompt the user to enter flight information
+    struct FlightInfo flights[10];                    // Declare an array of FlightInfo structs to store flight data
+    for (int f = 0; f < 10; ++f)
+    {
+        char dest[30];// array to store destination input
+        char dt[30];// array to store date input
+
+        printf("Enter destination for flight %d: ", f + 1);     // Prompt user for destination input
+        fgets(dest, sizeof(dest), stdin);
+        dest[strcspn(dest, "\n")] = '\0';               // Remove newline character from destination input
+
+        printf("Enter date for flight %d: ", f + 1);   // Prompt user for date input
+        fgets(dt, sizeof(dt), stdin);
+        dt[strcspn(dt, "\n")] = '\0';
+
+        fillFlightInfo(&flights[f], dest, dt);
+
+        printf("\n");
+    }
+
+    printf("\n");// Add a line break before printing flight information
+
+    printFlightInfo(flights, 10); // Print flight information
+
+    return 0;
+}
